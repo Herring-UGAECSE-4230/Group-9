e81ae0654f16409edcdb7f88b24bffe84dec6ddc
mnxp14 <phammichelle14@gmail.com>
2024-01-25 13:12:59 -0500

    Blinkled

--- BlinkLED/blinkled.py
+++ BlinkLED/blinkled.py
@@ -11,14 +11,15 @@ GPIO.setup(4,GPIO.OUT,initial=GPIO.LOW) #setup pin 4 as output
 
 # running while loop 
 while True:
+  
+  #LED on
   GPIO.output(4, GPIO.HIGH)   #set output to high
   print("LED is ON")
-  
-  sleep(0.04) # keep LED on for 0.04 second 
-  
-  GPIO.output(4,GPIO.LOW) # set LED output to low 
-  print("LED is off") 
+  sleep(0.07) # keep LED on for 0.04 second 
 
-  time.sleep(0.04) # keep LED for 0.04
+  #LED off
+  GPIO.output(4,GPIO.LOW) # set LED output to low 
+  print("LED is off")
+  time.sleep(0.07) # keep LED for 0.04
 
 
