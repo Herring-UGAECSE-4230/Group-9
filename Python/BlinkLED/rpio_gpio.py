e1b51a0c4c9a86408779de15eea8d80df39a6787
mnxp14 <phammichelle14@gmail.com>
2024-01-26 16:31:35 -0500

    pushes current files

--- Python/BlinkLED/rpio_gpio.py
+++ Python/BlinkLED/rpio_gpio.py
@@ -1,6 +1,6 @@
 # Part 1 RPi.GPIO 
 
-# import
+# imports necessary libraries
 import RPi.GPIO as GPIO
 import time
 from time import sleep
@@ -8,19 +8,16 @@ from time import sleep
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # calls the GPIO from the physical board 
-GPIO.setup(4,GPIO.OUT,initial=GPIO.LOW) #setup pin 4 as output
+GPIO.setup(23,GPIO.OUT,initial=GPIO.LOW) #setup pin 4 as output
 
-freq = 50   # sets a low frequency 
-
+freq = 100000   # sets a low frequency 
+print(f"{freq}")
 #running while loop 
 while True:
 
-  sleep(1/(freq * 2))  # delay for blinking 
-  GPIO.output(4, GPIO.HIGH)   #set output to high; turns ON LED 
-  print("LED is ON")   
-  
-  sleep(1/(freq * 2)) # delay for blinking 
-  GPIO.output(4,GPIO.LOW) # set LED output to low; turns OFF LED 
-  print("LED is off") 
-
-  GPIO.cleanup()
+  sleep(1/(freq*2))  # delay for blinking 
+  GPIO.output(23, GPIO.HIGH)   #set output to high; turns ON LED    
+    
+  sleep(1/(freq*2)) # delay for blinking 
+  GPIO.output(23,GPIO.LOW) # set LED output to low; turns OFF LED 
+GPIO.cleanup()
