809e46e1f511b67f115152a09fcdc88d3851b002
mnxp14 <phammichelle14@gmail.com>
2024-01-26 17:35:32 -0500

    pushes current code

--- Python/BlinkLED/pi_gpio.py
+++ Python/BlinkLED/pi_gpio.py
@@ -2,17 +2,15 @@ import pigpio
 import time 
 from time import sleep 
 
-pi=pigpio.pi() # selects the local pi for control with
-
-freq = 1000 # sets frequency variable 
+# pi=pigpio.pi('group9', 8889) # selects the local pi for control with
+pi = pigpio.pi()             # exit script if no connection
+if not pi.connected:
+   exit()
+freq = 100000 # sets frequency variable 
 pi.set_PWM_frequency(23, freq) # set GPIO 23 
-
+pi.set_PWM_dutycycle(23, 128) # PWM 1/2 on aka duty cycle=  %50 
+print(f"{freq}")
 
 while True: 
-    pi.set_PWM_dutycycle(23, 128) # set the duty cycle 50 % 
-    
     # empty loop that keeps program running while the LED is blinking.
-    pass 
-
-pi.set_PWM_dutycycle(23, 0) # set the pin frequency to 0 to shut it off 
- 
+    pass
