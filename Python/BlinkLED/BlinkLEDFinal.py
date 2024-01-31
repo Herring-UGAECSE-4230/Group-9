9b1a66a58547a7951d5ca4c4e2bda6b99d34f1af
mnxp14 <66038774+mnxp14@users.noreply.github.com>
2024-01-31 12:43:19 -0500

    Update BlinkLEDFinal.py

--- Python/BlinkLED/BlinkLEDFinal.py
+++ Python/BlinkLED/BlinkLEDFinal.py
@@ -1,1 +1,25 @@
+# Part 1 RPi.GPIO __________________________________________
 
+# imports necessary libraries
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) # calls the GPIO from the physical board 
+GPIO.setup(23,GPIO.OUT,initial=GPIO.LOW) #setup pin 4 as output
+
+freq = 100000   # sets a low frequency 
+print(f"{freq}")
+#running while loop 
+while True:
+
+  sleep(1/(freq*2))  # delay for blinking 
+  GPIO.output(23, GPIO.HIGH)   #set output to high; turns ON LED    
+    
+  sleep(1/(freq*2)) # delay for blinking 
+  GPIO.output(23,GPIO.LOW) # set LED output to low; turns OFF LED 
+GPIO.cleanup()
+
+# Part 2 Wiringpi _____________________________________________-
