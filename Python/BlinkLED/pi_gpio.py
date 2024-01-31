f4d14b110417fc77da9d6c0dd7caeef653051f2a
sofiafm008 <112444603+sofiafm008@users.noreply.github.com>
2024-01-31 11:10:29 -0500

    Updates pi_gpio.py

--- Python/BlinkLED/pi_gpio.py
+++ Python/BlinkLED/pi_gpio.py
@@ -2,10 +2,8 @@ import pigpio
 import time 
 from time import sleep 
 
-# pi=pigpio.pi('group9', 8889) # selects the local pi for control with
-pi = pigpio.pi()             # exit script if no connection
-if not pi.connected:
-   exit()
+pi = pigpio.pi()           
+
 freq = 100000 # sets frequency variable 
 pi.set_PWM_frequency(23, freq) # set GPIO 23 
 pi.set_PWM_dutycycle(23, 128) # PWM 1/2 on aka duty cycle=  %50 
