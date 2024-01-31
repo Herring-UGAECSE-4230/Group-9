05cf9df552b36e87de455119de2117a4f47ea086
mnxp14 <66038774+mnxp14@users.noreply.github.com>
2024-01-31 12:45:42 -0500

    Update BlinkLEDFinal.py

--- Python/BlinkLED/BlinkLEDFinal.py
+++ Python/BlinkLED/BlinkLEDFinal.py
@@ -39,3 +39,18 @@ while True:
 
 
 # Part 3 pigpio  _____________________________________________
+
+import pigpio
+import time 
+from time import sleep 
+
+pi = pigpio.pi()           
+
+freq = 100000 # sets frequency variable 
+pi.set_PWM_frequency(23, freq) # set GPIO 23 
+pi.set_PWM_dutycycle(23, 128) # PWM 1/2 on aka duty cycle= 50% 
+print(f"{freq}") # prints out the frequency
+
+while True: 
+    # empty loop that keeps program running while the LED is blinking.
+    pass
