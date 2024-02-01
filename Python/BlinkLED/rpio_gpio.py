fce65e31aa6b3d400bcc1c78e9eb3d434b25b632
mnxp14 <66038774+mnxp14@users.noreply.github.com>
2024-02-01 12:58:11 -0500

    Update rpio_gpio.py

--- Python/BlinkLED/rpio_gpio.py
+++ Python/BlinkLED/rpio_gpio.py
@@ -1,4 +1,4 @@
-# Part 1 RPi.GPIO 
+# Part 1 RPi.GPIO __________________________________________
 
 # imports necessary libraries
 import RPi.GPIO as GPIO
@@ -10,11 +10,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # calls the GPIO from the physical board 
 GPIO.setup(23,GPIO.OUT,initial=GPIO.LOW) #setup pin 4 as output
 
-<<<<<<< HEAD
-freq = 10   # sets a low frequency 
-=======
 freq = 5   # sets a low frequency 
->>>>>>> 4f6da97f341554a89b2fb8294933a7b2d2140dc4
 print(f"{freq}")
 #running while loop 
 while True:
