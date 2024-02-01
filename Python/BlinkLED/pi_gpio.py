527b330a203a91e2277608954ed13a1dceaa2e4c
mnxp14 <66038774+mnxp14@users.noreply.github.com>
2024-02-01 12:50:09 -0500

    Update pi_gpio.py

--- Python/BlinkLED/pi_gpio.py
+++ Python/BlinkLED/pi_gpio.py
@@ -2,7 +2,7 @@ import pigpio
 
 pi = pigpio.pi()           
 
-freq = 100000 # sets frequency variable 
+freq = 5 # sets frequency variable 
 pi.set_PWM_frequency(23, freq) # set GPIO 23 
 pi.set_PWM_dutycycle(23, 128) # PWM 1/2 on aka duty cycle=  %50 
 print(f"{freq}")
