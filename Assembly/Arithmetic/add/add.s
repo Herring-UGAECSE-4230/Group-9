1ca96a55d91775b93fb1b3b622364bec9629f4a5
sofiafm008 <sofiafm008@gmail.com>
2024-02-02 22:48:03 +0000

    adds all assmebly arithemtic files

--- Assembly/Arithmetic/add/add.s
+++ Assembly/Arithmetic/add/add.s
@@ -1,8 +1,8 @@
 .global _start
 
 _start:
-        @This stores 20 in R1 then subtracts 10 from that and stores the remaining amount in R0
+        @This stores 20 in R1 then adds 10 from that and stores the remaining amount in R0
         MOV R1, #0x14
-        ADD R0, R1, #0xA
+        ADDS R0, R1, #-0x14
         MOV R7, #1
         SWI 0
--- Assembly/Arithmetic/subtract/subtract.s
+++ Assembly/Arithmetic/subtract/subtract.s
@@ -3,6 +3,6 @@
 _start:
         @This stores 20 in R1 then subtracts 10 from that and stores the remaining amount in R0
         MOV R1, #0x14
-        SUB R0, R1, #0xA
+        SUBS R0, R1, #0x14
         MOV R7, #1
         SWI 0
