013b44de1cd3e5f5f1d65e279826d6c2cd53bac4
sofiafm008 <sofiafm008@gmail.com>
2024-02-03 20:28:54 +0000

    keypad

--- Python/7SD/keypad.py
+++ Python/7SD/keypad.py
@@ -1,111 +1,71 @@
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 
-# #setting row pins
-# ROW_1 = 26
-# ROW_2 = 19
-# ROW_3 = 13
-# ROW_4 = 6
+#setting row pins
+ROW_1 = 26
+ROW_2 = 19
+ROW_3 = 13
+ROW_4 = 6
 
-# #setting column pins
-# COL_1 = 5
-# COL_2 = 22
-# COL_3 = 27
-# COL_4 = 17
+#setting column pins
+COL_1 = 5
+COL_2 = 22
+COL_3 = 27
+COL_4 = 17
 
-# GPIO.setwarnings(False)
-# #BCM numbering
-# GPIO.setmode(GPIO.BCM)
-
+GPIO.setwarnings(False)
+#BCM numbering
+GPIO.setmode(GPIO.BCM)
 
 #from instructions: GPIO pins connected to the 'X' lines will be setup as inputs to the pad/output from the PI
-
-# #from instructions: GPIO pins connected to the 'X' lines will be setup as inputs to the pad/output from the PI
-
 # GPIO.setup(ROW_1, GPIO.IN)
 # GPIO.setup(ROW_2, GPIO.IN)
 # GPIO.setup(ROW_3, GPIO.IN)
 # GPIO.setup(ROW_4, GPIO.IN)
 
-
-# GPIO.setup(ROW_1, GPIO.OUT)
-# GPIO.setup(ROW_2, GPIO.OUT)
-# GPIO.setup(ROW_3, GPIO.OUT)
-# GPIO.setup(ROW_4, GPIO.OUT)
+GPIO.setup(ROW_1, GPIO.OUT)
+GPIO.setup(ROW_2, GPIO.OUT)
+GPIO.setup(ROW_3, GPIO.OUT)
+GPIO.setup(ROW_4, GPIO.OUT)
 
 #from instructions: pins connected to the 'Y' lines will be setup as outputs from the pad/inputs to the PI
 #if needed set low by default: pull_up_down=GPIO.PUD_DOWN
-# GPIO.setup(COL_1, GPIO.IN)
-# GPIO.setup(COL_2, GPIO.IN)
-# GPIO.setup(COL_3, GPIO.IN)
-# GPIO.setup(COL_4, GPIO.IN)
-
-
-
-# #from instructions: pins connected to the 'Y' lines will be setup as outputs from the pad/inputs to the PI
-# #if needed set low by default: pull_up_down=GPIO.PUD_DOWN
+GPIO.setup(COL_1, GPIO.IN)
+GPIO.setup(COL_2, GPIO.IN)
+GPIO.setup(COL_3, GPIO.IN)
+GPIO.setup(COL_4, GPIO.IN)
 
 # GPIO.setup(COL_1, GPIO.OUT)
 # GPIO.setup(COL_2, GPIO.OUT)
 # GPIO.setup(COL_3, GPIO.OUT)
 # GPIO.setup(COL_4, GPIO.OUT)
 
+def readKeypad(rowNum, char):
+    GPIO.output(rowNum, GPIO.HIGH)
+    if GPIO.input(COL_1)==1:
+        curVal = char[0]
+    if GPIO.input(COL_2)==1:
+        curVal = char[1]
+    if GPIO.input(COL_3)==1:
+        curVal = char[2]
+    if GPIO.input(COL_4)==1:
+        curVal = char[3]
+    GPIO.output(rowNum, GPIO.LOW)
+    return curVal #check this SIMLINE
 
-# def readKeypad(rowNum, char):
-#     curlVaL = 0
-#     GPIO.output(rowNum, GPIO.HIGH)
-#     if GPIO.input(COL_1)==1:
-#         curVal = char[0]
-#     if GPIO.input(COL_2)==1:
-#         curVal = char[1]
-#     if GPIO.input(COL_3)==1:
-#         curVal = char[2]
-#     if GPIO.input(COL_4)==1:
-#         curVal = char[3]
-#     GPIO.output(rowNum, GPIO.LOW)
-#     return curVal #check this SIMLINE
-
-# #physical keyboard layout
-# #loop checking each row
-# print("Press buttons on keypad. Ctrl+C to exit.")
-# try:
-#     while True:
-#         readKeypad(ROW_1,['1','2','3','A'])
-#         readKeypad(ROW_2,['4','5','6','B'])
-#         readKeypad(ROW_3,['7','8','9','C'])
-#         readKeypad(ROW_4,['*','0','#','D'])
-#         time.sleep(0.2)
-# except KeyboardInterrupt:
-#         print("\nKeypad Application Interrupted") 
-#         GPIO.cleanup()       
-
-# def readKeypad(rowNum, char):
-#     GPIO.output(rowNum, GPIO.HIGH)
-#     If GPIO.input(COL_1)==1:
-#         curVal = char[0]
-#     If GPIO.input(COL_2)==1:
-#         curVal = char[1]
-#     If GPIO.input(COL_3)==1:
-#         curVal = char[2]
-#     If GPIO.input(COL_4)==1:
-#         curVal = char[3]
-#     GPIO.output(rowNum, GPIO.LOW)
-#     return curVal #check this SIMLINE
-
-# #physical keyboard layout
-# #loop checking each row
-# print("Press buttons on keypad. Ctrl+C to exit.")
-# try:
-#     while True:
-#         readKeypad(ROW_1,[1,2,3,'A'])
-#         readKeypad(ROW_2,[4,5,6,'B'])
-#         readKeypad(ROW_3,[7,8,9,'C'])
-#         readKeypad(ROW_4,['*',0,'#','D'])
-#         time.sleep(0.2)
-# except KeyboardInterrupt:
-#         print("\nKeypad Application Interrupted") 
-#         GPIO.cleanup()       
-
+#physical keyboard layout
+#loop checking each row
+print("Press buttons on keypad. Ctrl+C to exit.")
+try:
+    while True:
+        readKeypad(ROW_1,['1','2','3','A'])
+        readKeypad(ROW_2,['4','5','6','B'])
+        readKeypad(ROW_3,['7','8','9','C'])
+        readKeypad(ROW_4,['*','0','#','D'])
+        time.sleep(0.2)
+except KeyboardInterrupt:
+        print("\nKeypad Application Interrupted") 
+        GPIO.cleanup()       
         
 
 
@@ -242,80 +202,3 @@
 #                 time.sleep(0.1)
 # except KeyboardInterrupt:
 #     print("\nApplication stopped!")
-
-
-import RPi.GPIO as GPIO
-import time
-
-# Setting row pins
-ROW_1 = 26
-ROW_2 = 19
-ROW_3 = 13
-ROW_4 = 6
-
-# Setting column pins
-COL_1 = 5
-COL_2 = 22
-COL_3 = 27
-COL_4 = 17
-
-GPIO.setwarnings(False)
-# BCM numbering
-GPIO.setmode(GPIO.BCM)
-
-# GPIO pins connected to the 'X' lines will be set up as inputs to the pad/output from the PI
-GPIO.setup(ROW_1, GPIO.IN)
-GPIO.setup(ROW_2, GPIO.IN)
-GPIO.setup(ROW_3, GPIO.IN)
-GPIO.setup(ROW_4, GPIO.IN)
-
-# Pins connected to the 'Y' lines will be set up as outputs from the pad/inputs to the PI
-# If needed set low by default: pull_up_down=GPIO.PUD_DOWN
-GPIO.setup(COL_1, GPIO.OUT)
-GPIO.setup(COL_2, GPIO.OUT)
-GPIO.setup(COL_3, GPIO.OUT)
-GPIO.setup(COL_4, GPIO.OUT)
-
-def readKeypad(rowNum, char):
-    GPIO.output(rowNum, GPIO.HIGH)
-    
-    curVal = None
-    
-    if GPIO.input(COL_1) == 1:
-        curVal = char[0]
-    elif GPIO.input(COL_2) == 1:
-        curVal = char[1]
-    elif GPIO.input(COL_3) == 1:
-        curVal = char[2]
-    elif GPIO.input(COL_4) == 1:
-        curVal = char[3]
-    
-    GPIO.output(rowNum, GPIO.LOW)
-    return curVal
-
-# Physical keyboard layout
-# Loop checking each row
-print("Press buttons on the keypad. Ctrl+C to exit.")
-try:
-    while True:
-        key_pressed = readKeypad(ROW_1, [1, 2, 3, 'A'])
-        if key_pressed is not None:
-            print(f"Key pressed: {key_pressed}")
-        
-        key_pressed = readKeypad(ROW_2, [4, 5, 6, 'B'])
-        if key_pressed is not None:
-            print(f"Key pressed: {key_pressed}")
-        
-        key_pressed = readKeypad(ROW_3, [7, 8, 9, 'C'])
-        if key_pressed is not None:
-            print(f"Key pressed: {key_pressed}")
-        
-        key_pressed = readKeypad(ROW_4, ['*', 0, '#', 'D'])
-        if key_pressed is not None:
-            print(f"Key pressed: {key_pressed}")
-        
-        time.sleep(0.2)
-
-except KeyboardInterrupt:
-    print("\nKeypad Application Interrupted")
-    GPIO.cleanup()
