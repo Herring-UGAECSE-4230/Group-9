2a22f239570de2a7cf1c90d225f5dec4a99c31b5
mnxp14 <66038774+mnxp14@users.noreply.github.com>
2024-02-08 13:36:02 -0500

    Update keypad.py

--- Python/7SD/keypad.py
+++ Python/7SD/keypad.py
@@ -1,35 +1,46 @@
 import RPi.GPIO as GPIO
 import time
 
-#setting row pins
-ROW_1 = 18
-ROW_2 = 23
-ROW_3 = 24
-ROW_4 = 25
+# #setting row pins
+# ROW_1 = 18
+# ROW_2 = 23
+# ROW_3 = 24
+# ROW_4 = 25
 
-#setting column pins
-COL_1 = 12
-COL_2 = 16
-COL_3 = 20
-COL_4 = 21
+# #setting column pins
+# COL_1 = 12
+# COL_2 = 16
+# COL_3 = 20
+# COL_4 = 21
+
+# Define the pin numbers for the keypad rows and columns
+keypad_rows = [18, 23, 24, 25] #X1-X4
+keypad_cols = [12, 16, 20, 21] #Y1-Y4
 
 GPIO.setwarnings(False)
 #BCM numbering
 GPIO.setmode(GPIO.BCM)
 
-#from instructions: GPIO pins connected to the 'X' lines will be setup as inputs to the pad/output from the PI
 
-GPIO.setup(ROW_1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ROW_2, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ROW_3, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ROW_4, GPIO.OUT, initial=GPIO.LOW)
+# Set up GPIO pins for keypad rows and columns
+for row_pin in keypad_rows:
+    GPIO.setup(row_pin, GPIO.OUT, initial = GPIO.LOW)
+for col_pin in keypad_cols:
+    GPIO.setup(col_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
+    # GPIO.output(col_pin, GPIO.HIGH)
+# #from instructions: GPIO pins connected to the 'X' lines will be setup as inputs to the pad/output from the PI
 
-#from instructions: pins connected to the 'Y' lines will be setup as outputs from the pad/inputs to the PI
-#if needed set low by default: pull_up_down=GPIO.PUD_DOWN
-GPIO.setup(COL_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COL_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COL_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COL_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(ROW_1, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(ROW_2, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(ROW_3, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(ROW_4, GPIO.OUT, initial=GPIO.LOW)
+
+# #from instructions: pins connected to the 'Y' lines will be setup as outputs from the pad/inputs to the PI
+# #if needed set low by default: pull_up_down=GPIO.PUD_DOWN
+# GPIO.setup(COL_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(COL_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(COL_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(COL_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def readKeypad(rowNum, char):
     GPIO.output(rowNum, GPIO.HIGH)
