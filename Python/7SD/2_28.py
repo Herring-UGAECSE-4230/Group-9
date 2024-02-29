c8e1e38ef56f7b48ccf5d6853a763fa0a5982701
sofiafm008 <sofiafm008@gmail.com>
2024-02-29 02:11:08 +0000

    current code

--- Python/7SD/2_28.py
+++ Python/7SD/2_28.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 from time import sleep
+import datetime
+import pytz
 
 GPIO.setwarnings(False)
 #BCM numbering
@@ -8,6 +10,7 @@ GPIO.setmode(GPIO.BCM)
 
 #global variables 
 global state, pressed, counter, new, invalid, waiting_for_valid_input, LED #state of each number being pressed; 0-14\
+global eastern, current_time, hour, minute_string, minute_digits_list
 
 #initialized variables
 state = -1 
@@ -15,8 +18,13 @@ pressed = -1
 counter = 0
 invalid = 0
 waiting_for_valid_input = 0
-LED = 0
 
+#initializes var associated with time
+eastern=pytz.timezone("US/Eastern") #sets eastern time zone
+current_time= datetime.datetime.now(eastern).time() #gets current time
+hour= current_time.hour #gets current hour
+minute_string= str(current_time.minute)#converts current minutes to string
+minute_digits_list = [int(digit) for digit in minute_string] #puts two digits of current minutes into list
 
 #setting row pins
 ROW_PINS = [18,23,24,25]
@@ -425,33 +433,132 @@ def start():
     #clkON()
     GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
     state=0
+    
+
+    
+def minute_fun():
+    global eastern, current_time, hour, minute_string, minute_digits_list
+    if len(minute_digits_list)==1:
+        minute_digits_list.insert(0,0) #add a 0 at index 0 to the list
+        print(minute_digits_list)
+    else:
+        print(minute_digits_list)
+
+def datetime_A():
+    global eastern, current_time, hour, minute_string, minute_digits_list
+    
+    
+    if hour==0: #it is 12 am
+        hour+=12
+        hour_string= str(current_time.hour) #converts current hour to string
+        hour_digits_list = [int(digit) for digit in hour_string] #makes a list of the hour digits
+        print(hour_digits_list)
+        minute_fun()
+    
+    elif hour==12: #it is 12pm             #add a dot indicating Pm!!!!!!!!!!!!!!!!!!!!!!!!!
+        hour_string= str(current_time.hour) #converts current hour to string
+        hour_digits_list = [int(digit) for digit in hour_string] #makes a list of the hour digits
+        print(hour_digits_list)
+        minute_fun()
+        
+       
+        
+    elif (0<hour<12): #it is the morning
+        hour_string= str(current_time.hour) #converts current hour to string
+        hour_digits_list = [int(digit) for digit in hour_string] #makes a list of the hour digits
+        if len(hour_digits_list)==1: #if only one digit is in the hour list 
+            hour_digits_list.insert(0,0) #add a 0 at index 0 to the list
+            print(hour_digits_list)
+            minute_fun()
+    
+        else:
+            print(hour_digits_list)
+            minute_fun()
+            
+    elif (hour>12): #it is the night            #add a dot indicating Pm!!!!!!!!!!!!!!!!!!!!!!!!!
+        hour-=12 #subtract 12
+        hour_string= str(current_time.hour) #converts current hour to string
+        hour_string=str(hour) #sets current hour not in military format to string 
+        hour_digits_list = [int(digit) for digit in hour_string] #makes a list of the hour digits
+        if len(hour_digits_list)==1: #if only one digit is in the hour list 
+            hour_digits_list.insert(0,0) #add a 0 at index 0 to the list
+            print(hour_digits_list)
+            minute_fun()
+        else:
+            print(hour_digits_list)
+            minute_fun()
+    
+    combined_list = hour_digits_list + minute_digits_list
+    return combined_list
+            
+    #return minute_digits_list
 
 print("Press buttons on keypad. Ctrl+C to exit.")
 
 try:
+#     datetime_A()
     clkON()
     #reset()
     #start()
     #clkReset()
     GPIO.output(CLK_PINS[0], GPIO.HIGH)
     GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
+
+    combined_list = datetime_A()
+    ssd_1 = combined_list[0]
+    ssd_2 = combined_list[1]
+    ssd_3 = combined_list[2]
+    ssd_4 = combined_list[3]
+
+    
+    
     while True:
-      
-       
+        
+        
+            
+        
         while counter != 4: # while count is no equal to 4, will run the if statements
-            blink(CLK_PINS[counter])
+#             blink(CLK_PINS[counter])
             
             if counter == 0: # when counter = 0 -> corresponds to SSD1
                 pressed = -1
+                print(f"state: {state}")
+                if state==0:
+                    GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
+                if state==1: # if state == (0-14) then it will call each numbers function to turn the GPIO segments ON 
+                    GPIO.output([22,13], GPIO.HIGH)
+                if state==2:
+                    GPIO.output([27,22,3,5,6], GPIO.HIGH)
+                if state==3:
+                    GPIO.output([27,22,3,13,6], GPIO.HIGH)
+                if state==4:
+                    GPIO.output([2,3,22,13], GPIO.HIGH)
+                if state==5:
+                    GPIO.output([27,2,3,13,6], GPIO.HIGH)
+                if state==6:
+                    GPIO.output([27,2,5,6,13,3], GPIO.HIGH)
+                if state==7:
+                    GPIO.output([27,22,13], GPIO.HIGH)
+                if state==8:
+                    GPIO.output([27,22,13,2], GPIO.HIGH)
+                if state==9:
+                    GPIO.output([27,2,22,3,13], GPIO.HIGH)
+                if state==10:
+                    GPIO.output(26, GPIO.HIGH)
+            
                     
                 displaySSD(CLK_PINS[0]) #calls displaySSD function to display on SSD
+                
+                
+                
                 GPIO.output(CLK_PINS[0], GPIO.LOW) # turns clk1 off 
                 print(f"this is the counter {counter}")
                 sleep(0.15)
                
             if counter == 1:
                 pressed = -1
-                
+                state = ssd_2
+                print(f"state: {state}")
                 GPIO.output(CLK_PINS[0], GPIO.HIGH) #stores the value of the SSD1
                 if state==0:
                     GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
@@ -492,7 +599,8 @@ try:
                 
             if counter == 2:
                 pressed = -1
-                
+                state = ssd_3
+                print(f"state: {state}")
                 GPIO.output(CLK_PINS[1], GPIO.HIGH) #stores the value of the SSD2
                 if state==0:
                     GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
@@ -537,7 +645,8 @@ try:
                 
             if counter == 3:
                 pressed = -1
-                
+                state = ssd_4
+                print(f"state: {state}")
                 GPIO.output(CLK_PINS[2], GPIO.HIGH) #stores the value of the SSD3
                 if state==0:
                     GPIO.output([27,22,13,2,5,6,26], GPIO.HIGH)
@@ -588,7 +697,6 @@ try:
 except KeyboardInterrupt:
         print("\nKeypad Application Interrupted") 
         GPIO.cleanup()       
-
 
 
 
