4e4faf4365c80c00e4653d82e6271d54164a35d1
sofiafm008 <sofiafm008@gmail.com>
2024-03-14 01:49:23 +0000

    encoder

--- Python/MOSFET Motor Driver with Digital Encoder/callback.py
+++ Python/MOSFET Motor Driver with Digital Encoder/callback.py
@@ -1,99 +1,42 @@
-import pigpio
-from pigpio_encoder.rotary import Rotary
+import RPi.GPIO as GPIO
+import time
 
+#GPIO pin set up
+clk=13
+dt=6
+sw=5
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sw, GPIO.IN)
 
-pi = pigpio.pi()
+#initialize the counter and the encoder turn
+counter=0
+lastClkState=GPIO.input(clk)
 
+#debouncing method, without one click would increment counter by 2
+def debounce():
+    time.sleep(.5)
 
-last_counter = 0  # Initialize last_counter outside the function
+while True:
+    debounce()
 
-def rotary_callback_x(counter):
-    global last_counter
-
-    # print("Counter value:", counter)
-
-    if counter > last_counter:
-        last_counter = counter
-            # print("clockwise + 10")
-        up_callback_x(16)
-    elif counter < last_counter:
-        last_counter = counter
-        if pi.read(9)==1:
-            return
+    #checks states if the pins
+    clkState=GPIO.input(clk)
+    dtState=GPIO.input(dt)
+    #if statement to check for a turn
+    if clkState!=lastClkState:
+        if dtState!=clkState:
+            print("clockwise")
+            counter+=1
+            debounce()
+#             time.sleep(10)
         else:
-        # print("counterclockwise - 10")
-            down_callback_x(16)
-    else:
-        print("else of x")
-
-
-def rotary_callback_y(counter):
-    global last_counter
-
-    # print("Counter value:", counter)
-
-    if counter > last_counter:
-        last_counter = counter
-        if pi.read(10)==1:
-            return
-        else:
-            up_callback_y(16)
-        # print("clockwise + 10")
-    elif counter < last_counter:
-        last_counter = counter
-        down_callback_y(16)
-        # print("counterclockwise - 10")
-    else:
-        print("else of y")
-
-global state
-state=1
-def sw_short_y():
-    global state
-    if state==1:
-        pen_down(200)
-        state=2
-        return
-    else:
-        pen_up(200)
-        state=1
-
-
-def up_callback_x(x):
-    for _ in range(x):
-        motor_x.doClockwiseStep()
-def up_callback_y(x):
-    for _ in range(x):
-        motor_y.doClockwiseStep()
-
-def down_callback_x(x):
-    for _ in range(x):
-        motor_x.doCounterclockwiseStep()
-def down_callback_y(x):
-    for _ in range(x):
-        motor_y.doCounterclockwiseStep()
-
-
-my_rotary_x = Rotary(
-    clk_gpio=22,
-    dt_gpio=27,
-    sw_gpio=17
-)
-my_rotary_x.setup_rotary(
-    min=0,
-    max=10000,
-    scale=1,
-    debounce=200,
-    rotary_callback=rotary_callback_x
-)
-my_rotary_x.setup_switch(
-    debounce=200,
-    long_press=True,
-    sw_short_callback=sw_short
-)
-
-my_rotary_x.watch()
-
+            print("counterclockwise")
+            counter-=1
+        lastClkState=clkState #update last clk state
+        print(counter)
+        debounce()
 
 
 
--- Python/MOSFET Motor Driver with Digital Encoder/test.py
+++ Python/MOSFET Motor Driver with Digital Encoder/test.py
@@ -1,10 +1,11 @@
 import pigpio
-from xyimport import StepperMotor
+import RPi.GPIO
+# from xyimport import StepperMotor
 from pigpio_encoder.rotary import Rotary
 
 last_counter = 0  # Initialize last_counter outside the function
 pi = pigpio.pi()
-motor = StepperMotor(pi, 23, 24, delayAfterStep = .001) 
+# motor = StepperMotor(pi, 23, 24, delayAfterStep = .001) 
 
 def rotary_callback(counter):
     global last_counter
@@ -16,13 +17,30 @@ def rotary_callback(counter):
         last_counter = counter
         print("counterclockwise - 1")
     else:
-        print("else")
+        print("none")
 
 def sw_short():
     print("Switch short press")
 
 def sw_long():
     print("Switch long press")
+
+clk_gpio=13
+GPIO.setup(clk_gpio,GPIO.IN, pull_up_down = GPIO.PUD_UP) #later on5
+def stateCheck():
+    count = 0
+    lastClkState = GPIO.input(clk_gpio)
+    while True:
+        clkState = GPIO.input(clk_gpio)
+        dtState = GPIO.input(dt_gpio)
+        if clkState != lastClkState:
+            if dtState != clkState:
+                count+=1
+                print("clockwise + 1")
+            else:
+                count+=1
+        lastClkState=clkState
+        print(count)
 
 #sets up the rotary encoder pins
 my_rotary = Rotary(
@@ -37,7 +55,8 @@ my_rotary.setup_rotary(
     max=1100,
     scale=1,
     debounce=200,
-    rotary_callback=rotary_callback
+    #rotary_callback=rotary_callback
+    rotary_callback=stateCheck
 )
 
 #sets up the call backs for if the switch is short pressed and long pressed
@@ -49,3 +68,6 @@ my_rotary.setup_switch(
 )
 
 my_rotary.watch()
+
+
+
